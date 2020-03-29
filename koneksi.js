6575d694a41904038312614140a866d5b43f48a9
danikusuma10 <kusumadanikusuma@outlook.com>
2020-03-29 13:27:50 +0700

    buat kode res

--- koneksi.js
+++ koneksi.js
@@ -2,9 +2,10 @@ var mysql = require('mysql');
 
 //koneksi db
 const conn = mysql.createConnection({
-    host:'',
-    user:'',
-    database:''
+    host:'206.189.94.93',
+    user:'progclas_dani',
+    password:'embossy1!',
+    database:'progclas_dani'
 });
 
 conn.connect((err)=>{
