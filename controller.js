e9d7ae0e5b554e7c61517c9cf1803321687e6d4f
danikusuma10 <kusumadanikusuma@outlook.com>
2020-03-29 15:01:26 +0700

    berhasil tampil data user

--- controller.js
+++ controller.js
@@ -6,3 +6,17 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("My app RestApi work",res)
 };
+
+
+//tampil data di db
+exports.tampilsemuauser = function(req,res){
+    connection.query('SELECT * FROM tbl_user', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }
+        else
+        {
+            response.ok(rows, res)
+        }
+    });
+};
